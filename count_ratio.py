6024f1a8c526aac19d3d2e662b016639b1274a3a
darkmarbo <darkmarbo@163.com>
2016-12-07 14:42:07 +0800

    modify count_ratio.py

--- count_ratio.py
+++ count_ratio.py
@@ -16,8 +16,11 @@ ii =0;
 all_line_in=fp_in.readlines()
 for line in all_line_in:
     ii += 1;
-    if ('\n' == line[-1] ):
+    if len(line)>1 and '\r\n' == line[-2:]:
+        line = line[:-2]
+    elif ('\n' == line[-1] ):
         line = line[:-1]
+
 
     line = line.lower();
     dict_in[line] = 1;
@@ -56,30 +59,30 @@ for line in fp_in_cankao:
             list_top_num[0] += 1;
         if (ii<10000):
             list_top_num[1] += 1;
-        if (ii<20000):
+        if (ii<15000):
             list_top_num[2] += 1;
-        if (ii<30000):
+        if (ii<20000):
             list_top_num[3] += 1;
-        if (ii<40000):
+        if (ii<30000):
             list_top_num[4] += 1;
-    elif (ii<20000):
+    elif (ii<15000):
         fp_out.write("%s\n"%(word))
 
 
 list_top_num[0] = list_top_num[0]/5000.0;
 list_top_num[1] = list_top_num[1]/10000.0;
-list_top_num[2] = list_top_num[2]/20000.0;
-list_top_num[3] = list_top_num[3]/30000.0;
-list_top_num[4] = list_top_num[4]/40000.0;
+list_top_num[2] = list_top_num[2]/15000.0;
+list_top_num[3] = list_top_num[3]/20000.0;
+list_top_num[4] = list_top_num[4]/30000.0;
 
 
 ratio = float(num_all)/float(num_all_cankao);
-fp_out.write("ratio:%.4f\n"%(ratio))
+#fp_out.write("ratio:%.4f\n"%(ratio))
 fp_out.write("top 5000 ratio:%.4f\n"%(list_top_num[0]))
 fp_out.write("top 10000 ratio:%.4f\n"%(list_top_num[1]))
-fp_out.write("top 20000 ratio:%.4f\n"%(list_top_num[2]))
-fp_out.write("top 30000 ratio:%.4f\n"%(list_top_num[3]))
-fp_out.write("top 40000 ratio:%.4f\n"%(list_top_num[4]))
+fp_out.write("top 15000 ratio:%.4f\n"%(list_top_num[2]))
+fp_out.write("top 20000 ratio:%.4f\n"%(list_top_num[3]))
+fp_out.write("top 30000 ratio:%.4f\n"%(list_top_num[4]))
  
 
 
